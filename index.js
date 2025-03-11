2e1b2975d19f896044e0a952dd3e77ad4273d32d
Aditya Pandey <adityapandey141@gmail.com>
2025-03-11 11:25:12 +0530

    added cors

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 const port = 3010;
 
 app.use(express.static("static"));
+
+let cors = require("cors");
+
+app.use(cors());
 const trackAPI = require("./tracksAPI.js");
 
 app.get("/", (req, res) => {
